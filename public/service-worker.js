8f5343ce5afa89c7b3ef57b2ca75d880b53077ee
Jake Studer <jacob.studer.dev@gmail.com>
2021-07-03 18:57:50 -0600

    added manifest, added service worker functionality, app is working

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,12 @@
 const FILES_TO_CACHE = [
   '/',
   '/index.html',
-  '/public/style.css',
-  '/public/dist/index.bundle.js',
+  '/styles.css',
+  '/index.js',
+  '/db.js',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png',
   'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
   'https://cdn.jsdelivr.net/npm/chart.js@2.8.0',
 ];
